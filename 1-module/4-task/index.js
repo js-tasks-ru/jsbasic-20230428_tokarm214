3703818560bf4c19b154ec69a15dd4785f1d8997
M1xSerM4s <tokarm214@gmail.com>
2023-05-04 23:43:20 +0300

    1.1-1.5 tasks completed 04.05.23

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,14 @@
 function checkSpam(str) {
-  // ваш код...
+  const FIRST_CHECK_VALUE = "1xBet";
+  const SECOND_CHECK_VALUE = "XXX";
+
+  str = str.toLowerCase();
+  if (
+    str.includes(FIRST_CHECK_VALUE.toLowerCase()) ||
+    str.includes(SECOND_CHECK_VALUE.toLowerCase())
+  ) {
+    return true;
+  }
+
+  return false;
 }
