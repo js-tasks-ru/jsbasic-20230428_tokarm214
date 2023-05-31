31361c424c5d44dcff17dd4d616d9f4caf0c1ae7
M1xSerM4s <tokarm214@gmail.com>
2023-05-31 21:43:04 +0300

    Module 4 completed

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
 function makeFriendsList(friends) {
-  // ваш код...
+	const friendsList = document.createElement("ul");
+
+	friends.forEach((person) => {
+		const fullName = document.createElement("li");
+
+		fullName.textContent = `${person.firstName} ${person.lastName}`;
+		friendsList.append(fullName);
+	});
+	
+	return friendsList;
 }
